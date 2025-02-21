de3835a2e6d29311c8281a92d0ca522846a1ff95
GuntreddyHemanth <guntreddyhemanth@gmail.com>
2025-02-21 19:55:33 +0530

    signup page

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import { Quote } from "../components/Quote"
+
+
+export const Signup = () => {
+    return <div className=" grid grid-cols-2">
+        <div>
+            hi
+        </div>
+        <Quote/>
+    </div>
+}
